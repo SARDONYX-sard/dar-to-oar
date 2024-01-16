f9bbcb7b1f3a5c4adaf73ee7cd69a5958ca14319
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-01-17 01:26:16 +0900

    chore(sample): change sample script

--- test/sample_scripts/multi-mod-conversion.ps1
+++ test/sample_scripts/multi-mod-conversion.ps1
@@ -1,5 +1,4 @@
-function Convert-Mods($base, $mods_dir, $log_level) {
-  <# Example dir status
+<# Example dir status
 D:/Programming/rust/dar-to-oar
   ├─── test
   │     └─── data
@@ -8,6 +7,17 @@ D:/Programming/rust/dar-to-oar
   └─── logs
 #>
 
+cargo build --release
+
+$base_dir = "D:/Programming/rust/dar-to-oar" # Convert target base directory
+$bin_dir = "D:/Programming/rust/dar-to-oar/target/release" # Directory with dar2oar.exe
+
+if (!$(Get-Command dar2oar -ErrorAction SilentlyContinue)) {
+  # Temporarily pass through to access without specifying an absolute path.
+  $env:Path += ";$bin_dir"
+}
+
+function Convert-Mods($base, $mods_dir, $log_level) {
   # Create log dir if it doesn't exist.
   if (!$(Test-Path "$base_dir/logs")) {
     New-Item -ItemType Directory "$base_dir/logs"
@@ -22,8 +32,7 @@ D:/Programming/rust/dar-to-oar
     # The following values are expected for `$_.Name`.
     # - Modern Female Sitting Animations Overhaul
     # - UNDERDOG Animations
-    cargo run --release -- `
-      convert $_.FullName `
+    dar2oar convert $_.FullName `
       --run-parallel `
       --stdout `
       --log-level $log_level `
@@ -39,8 +48,7 @@ function Show-Dar($base, $mods_dir, $log_level) {
 
   Get-ChildItem $mods_dir -Directory |
   ForEach-Object {
-    cargo run --release -- `
-      unhide-dar $_.FullName `
+    dar2oar unhide-dar $_.FullName `
       --stdout `
       --log-level $log_level `
       --log-path "$base_dir/logs/convert-$($_.Name).log"
@@ -55,23 +63,23 @@ function Remove-Oar($base, $mods_dir, $log_level) {
 
   Get-ChildItem $mods_dir -Directory |
   ForEach-Object {
-    cargo run --release -- `
-      remove-oar $_.FullName `
+    dar2oar remove-oar $_.FullName `
       --stdout `
       --log-level $log_level `
       --log-path "$base_dir/logs/convert-$($_.Name).log"
     Write-Host ""
   }
 }
+
 function Get-Help() {
-  cargo run --release -- --help
-  cargo run --release -- convert --help
-  cargo run --release -- remove-oar --help
-  cargo run --release -- unhide-dar --help
+  dar2oar --help
+  dar2oar --help
+  dar2oar convert --help
+  dar2oar remove-oar --help
+  dar2oar unhide-dar --help
 }
 
-$base_dir = "D:/Programming/rust/dar-to-oar" # Convert target base directory
 # Convert-Mods $base_dir "$base_dir/test/data" "debug"
-Remove-Oar  $base_dir "$base_dir/test/data" "debug"
-Show-Dar $base_dir "$base_dir/test/data" "debug"
-# Get-Help
+# Remove-Oar  $base_dir "$base_dir/test/data" "debug"
+# Show-Dar $base_dir "$base_dir/test/data" "debug"
+Get-Help
