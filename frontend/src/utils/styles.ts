809dd4cdfeb876028cf9bf611bcb9fff5022baac
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-01-18 12:55:56 +0900

    fix(front-style): more clear background color

--- frontend/src/utils/styles.ts
+++ frontend/src/utils/styles.ts
@@ -1,5 +1,15 @@
-function createPreset() {
-  return `
+/**
+ * Creates a CSS preset.
+ *
+ * @template T - The type of the CSS string.
+ * @param {T} css - The CSS string.
+ * @returns {`${T} as const`} - The CSS preset as a readonly string literal type.
+ */
+function createPreset<T extends string>(css: T) {
+  return /* css */ `:root {
+  ${css}
+}
+
 body {
   background-attachment: fixed;
   background-color: #000;
@@ -68,58 +78,55 @@ label.Mui-focused,
 
 .MuiLinearProgress-bar {
   background-color: var(--theme-color);
-}` as const;
+}
+` as const;
 }
 
-const preset1 = `:root {
-  --autofill-color: #691c3747;
+const preset1 = createPreset(
+  `--autofill-color: #691c3747;
   --convert-btn-color: #ab2b7e6e;
   --error-color: #ff1655;
   --hover-btn-color: #ff89898b;
   --hover-convert-btn-color: #fd3b3b6e;
   --image-size: cover;
   --image-url: url("https://i.redd.it/red-forest-1920-1080-v0-s9u8ki2rr70a1.jpg?s=139edf608c428656505a143635a0687dec086229");
-  --main-bg-color: #2226;
-  --theme-color: #ff8e16;
-}
-${createPreset()}` as const;
+  --main-bg-color: #2223;
+  --theme-color: #ff8e16;` as const,
+);
 
-const preset2 = `:root {
-  --autofill-color: #5eb1ef24;
+const preset2 = createPreset(
+  `--autofill-color: #5eb1ef24;
   --convert-btn-color: #3369ad7d;
   --hover-btn-color: #1d5aa58b;
-  --hover-convert-btn-color: #153257cc;
+  --hover-convert-btn-color: #2665b5d1;
   --image-size: cover;
   --image-url: url("https://images.pexels.com/photos/2817421/pexels-photo-2817421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750& dpr=1");
-  --main-bg-color: #2228;
-  --theme-color: #5a9ab9;
-}
-${createPreset()}` as const;
+  --main-bg-color: #2223;
+  --theme-color: #5a9ab9;` as const,
+);
 
-const preset3 = `:root {
-  --autofill-color: #eb37ff1c;
+const preset3 = createPreset(
+  `--autofill-color: #eb37ff1c;
   --convert-btn-color: #ab2b7e6e;
   --hover-btn-color: #8b51fb8b;
   --hover-convert-btn-color: #7d00c9a3;
   --image-size: cover;
   --image-url: url("https://images.pexels.com/photos/6162265/pexels-photo-6162265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
-  --main-bg-color: #2227;
-  --theme-color: #9644f1;
-}
-${createPreset()}` as const;
+  --main-bg-color: #2223;
+  --theme-color: #9644f1;` as const,
+);
 
-const preset4 = `:root {
-  --autofill-color: #a19c0038;
+const preset4 = createPreset(
+  `--autofill-color: #a19c0038;
   --convert-btn-color: #94ce7c6e;
   --hover-btn-color: #cefb518b;
   --hover-convert-btn-color: #81c462a3;
   --image-position-x: center;
   --image-position-y: center;
   --image-url: url('https://images.pexels.com/photos/973324/pexels-photo-973324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
-  --main-bg-color: #2229;
-  --theme-color: rgb(185, 185, 90);
-}
-${createPreset()}` as const;
+  --main-bg-color: #2228;
+  --theme-color: rgb(185, 185, 90);` as const,
+);
 
 export const presetStyles = {
   '1': preset1,
