5758b2b04f1ec83d379e2590eecc5e514709c328
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-03-13 13:11:06 +0900

    feat(condition_parser): support for argument inversion

--- core/src/condition_parser/compare.rs
+++ core/src/condition_parser/compare.rs
@@ -3,7 +3,7 @@ use super::errors::{ParseError, Result};
 use crate::{
     conditions::{CompareValues, ConditionSet},
     dar_syntax::FnArgs,
-    values::{Cmp, NumericValue, PluginValue},
+    values::Cmp,
 };
 
 /// Parses a comparison-based condition for plugin values.
@@ -16,15 +16,15 @@ pub(super) fn parse_compare<'a>(
     mut args: FnArgs<'a>,
     negated: bool,
 ) -> Result<ConditionSet<'a>> {
-    let plugin_value: PluginValue = args.pop_front()?.try_into()?;
-    let static_value = args.pop_front()?.try_into()?;
+    let value_a = args.pop_front()?;
+    let value_b = args.pop_front()?;
 
     let create_compare = |comparison: Cmp| {
         ConditionSet::CompareValues(CompareValues {
             negated,
-            value_a: NumericValue::GlobalVariable(plugin_value.into()),
+            value_a: value_a.into(),
             comparison,
-            value_b: NumericValue::StaticValue(static_value),
+            value_b: value_b.into(),
             ..Default::default()
         })
     };
@@ -39,4 +39,83 @@ pub(super) fn parse_compare<'a>(
             })
         }
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        dar_syntax::{ast::fn_args::fn_args, FnArg, NumberLiteral},
+        values::{Cmp, NumericValue, PluginValue, StaticValue},
+    };
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_parse_value_less_than() {
+        // test inputs
+        let condition_name = "ValueLessThan";
+        let args = fn_args![
+            FnArg::Number(NumberLiteral::Float(3.5)), // compare value
+            FnArg::PluginValue {
+                plugin_name: "Skyrim.esm",
+                form_id: NumberLiteral::Hex(0x0000_0007),
+            }
+        ];
+        let is_negated = false;
+
+        let result = parse_compare(condition_name, args, is_negated);
+
+        let expected = ConditionSet::CompareValues(CompareValues {
+            negated: false,
+            value_a: NumericValue::StaticValue(StaticValue { value: 3.5 }),
+            comparison: Cmp::Le,
+            value_b: NumericValue::GlobalVariable(
+                PluginValue {
+                    plugin_name: "Skyrim.esm".into(),
+                    form_id: "7".into(),
+                }
+                .into(),
+            ),
+            ..Default::default()
+        });
+
+        match result {
+            Ok(result) => {
+                assert_eq!(result, expected);
+            }
+            Err(err) => panic!("{err}"),
+        }
+    }
+
+    #[test]
+    fn test_parse_less_than_inverse() {
+        let condition_name = "ValueEqualTo";
+        let args = fn_args![
+            FnArg::PluginValue {
+                plugin_name: "Skyrim.esm",
+                form_id: NumberLiteral::Hex(0x0000_0007),
+            },
+            FnArg::Number(NumberLiteral::Float(3.5)) // compare value
+        ];
+
+        let is_negated = true;
+
+        let result = parse_compare(condition_name, args, is_negated);
+
+        let expected = Ok(ConditionSet::CompareValues(CompareValues {
+            negated: true,
+            value_a: NumericValue::GlobalVariable(
+                PluginValue {
+                    plugin_name: "Skyrim.esm".into(),
+                    form_id: "7".into(),
+                }
+                .into(),
+            ),
+            comparison: Cmp::Eq,
+            value_b: NumericValue::StaticValue(StaticValue { value: 3.5 }),
+            ..Default::default()
+        }));
+
+        assert_eq!(result, expected);
+    }
 }
