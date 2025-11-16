1442d5b0190b2f1c8efca303541c78884109ce5a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-11-16 12:26:01 +0900

    chore(clippy): remove duplicate lint

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -73,7 +73,6 @@
     clippy::string_add,
     clippy::string_add_assign,
     clippy::string_lit_as_bytes,
-    clippy::string_to_string,
     clippy::suspicious_operation_groupings,
     clippy::trait_duplication_in_bounds,
     clippy::unnested_or_patterns,
